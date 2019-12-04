3f068a890444ac1361b0b3dd647c563f8e45d5bf
pocethereum <58019889+pocethereum@users.noreply.github.com>
2019-12-04 16:00:35 +0800

    Update env.sh

--- build/env.sh
+++ build/env.sh
@@ -7,14 +7,6 @@ if [ ! -f "build/env.sh" ]; then
     exit 2
 fi
 
-# Create fake Go workspace if it doesn't exist yet.
-workspace="$PWD/build/_workspace"
-
-
-# Set up the environment to use the workspace.
-GOPATH="$workspace":"/Users/bing/"
-export GOPATH
-
 
 # Launch the arguments with the configured environment.
 exec "$@"
